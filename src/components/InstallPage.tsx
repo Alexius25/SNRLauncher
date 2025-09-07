6602c13da8e8d2103f86fe9802714d79cde8eb03
Alexius25 <171688835+Alexius25@users.noreply.github.com>
2025-09-07 13:47:05 +0200

    Added Background

--- src/components/InstallPage.tsx
+++ src/components/InstallPage.tsx
@@ -1,18 +1,43 @@
 import React from "react";
-import { FaDownload } from "react-icons/fa6";
+import { MdFileDownload } from "react-icons/md";
+import { FaDiscord } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
 
 function InstallPage() {
     return (
-        <div className="flex flex-col h-screen">
-            {/* Bild immer oben */}
-            <div className="flex justify-center">
-                <img className="p-6" src="/public/subnautica-logo-snr.png" alt="Installation" />
+        <div className="relative flex flex-col h-screen">
+        {/* Background Image */}
+            <div className="absolute inset-0 -z-10 pointer-events-none">
+                <img className="w-full h-full object-cover" src="/background.jpg" alt="Background" />
             </div>
 
-            {/* Button immer unten */}
-            <div className="flex flex-col items-center mt-auto mb-6">
-                <button className="bg-green-500 text-center flex items-center gap-x-2 p-2 rounded">
-                    <FaDownload />
+            {/* Top-Bar */}
+            <div className="fixed top-2 left-0 right-0 w-[95%] h-12 bg-gray-600 z-10 flex justify-between items-center px-4 shadow-3xl mx-auto rounded-lg">
+                {/* Logo left */}
+                <a href="https://369studios.tech/" target="_blank" className="p-1 rounded-full transition-colors duration-200 ease-in-out cursor-pointer hover:bg-white/10 flex items-center justify-center">
+                    <img className="h-8 rounded-full" src="/SNR.jpg" alt="Logo" />
+                </a>
+
+                {/* Buttons right */}
+                <div className="flex space-x-2">
+                    <a href="https://discord.gg/cnErGuf6UT" target="_blank" className="text-2xl p-2 rounded-full transition-colors duration-200 ease-in-out cursor-pointer hover:bg-white/10 flex items-center justify-center">
+                        <FaDiscord />
+                    </a>
+                    <a href="https://github.com/369-Studios" target="_blank" className="text-2xl p-2 rounded-full transition-colors duration-200 ease-in-out cursor-pointer hover:bg-white/10 flex items-center justify-center">
+                        <IoLogoGithub />
+                    </a>
+                </div>
+            </div>
+
+            {/* Logo */}
+            <div className="flex justify-center z-10 mt-8">
+                <img className="p-6" src="/subnautica-logo-snr.png" alt="Installation" />
+            </div>
+
+            {/* Install-Button */}
+            <div className="flex flex-col items-center mt-auto mb-6 z-10">
+                <button className="w-3xs text-center bg-gray-500 cursor-pointer text-white flex items-center justify-center gap-x-2 p-2 rounded hover:bg-green-600 transition-colors duration-200 ease-in-out">
+                    <MdFileDownload className="text-2xl" />
                     <span>Install</span>
                 </button>
             </div>
